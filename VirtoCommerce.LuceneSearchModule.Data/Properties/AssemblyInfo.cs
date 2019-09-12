709ad88188a61141c7b32a951756478f9db6dc54
Andrei Kostyrin <kostyrin2@gmail.com>
2019-09-12 12:22:33 +0200

    #vp464 V3 solution and projects

--- VirtoCommerce.LuceneSearchModule.Data/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("VirtoCommerce.LuceneSearchModule.Data")]
-[assembly: AssemblyDescription("")]
--- VirtoCommerce.LuceneSearchModule.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("VirtoCommerce.LuceneSearchModule.Test")]
-[assembly: AssemblyDescription("")]
