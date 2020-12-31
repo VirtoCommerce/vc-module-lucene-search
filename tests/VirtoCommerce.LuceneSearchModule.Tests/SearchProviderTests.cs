110db4272881c513c909ebdcdc5f416272221866
Aleksandr Andreev <alivemenone@gmail.com>
2020-12-31 10:05:50 +0300

    VP-1002: Run integration tests on github actions

--- tests/VirtoCommerce.LuceneSearchModule.Tests/SearchProviderTests.cs
+++ tests/VirtoCommerce.LuceneSearchModule.Tests/SearchProviderTests.cs
@@ -7,7 +7,7 @@ using Xunit;
 namespace VirtoCommerce.LuceneSearchModule.Tests
 {
     [TestCaseOrderer(PriorityTestCaseOrderer.TypeName, PriorityTestCaseOrderer.AssembyName)]
-    [Trait("Category", "IntegrationTest")]
+    //[Trait("Category", "IntegrationTest")]
     public abstract class SearchProviderTests : SearchProviderTestsBase
     {
         public const string DocumentType = "item";
